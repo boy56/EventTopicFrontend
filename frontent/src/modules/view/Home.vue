40f37f04de53a97e3e78fd505c6839c5a01aed37
ningyx-buaa <947208885@qq.com>
2020-04-22 09:27:26 +0800

    file

--- frontent/src/modules/view/Home.vue
+++ frontent/src/modules/view/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="view-page-wrapper" v-title data-title="专家观点">
-    <v-header></v-header>
+    <!-- <v-header></v-header> -->
     <v-filter-tab @update:filter="updateFilter"></v-filter-tab>
-    <v-search-box :search-input.sync="searchInput"></v-search-box>
+    <!-- <v-search-box :search-input.sync="searchInput"></v-search-box> -->
     <v-list :disp-datas="dispDatas"
             :fetch-simitems-cb="fetchSimNewsById"></v-list>
 
